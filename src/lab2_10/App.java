dce8b6980b1fe3f4d3427bbef36ede9ed41d3bd2
D1 <botgen@gmail.com>
2018-10-30 02:02:57 +0200

    Lab DONE

---
+++ src/lab2_10/App.java
@@ -0,0 +1,16 @@
+package lab2_10;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int num;
+        int l;
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter lenght of array: ");
+        l = sc.nextInt();
+        System.out.println("Y");
+
+
+    }
+}
